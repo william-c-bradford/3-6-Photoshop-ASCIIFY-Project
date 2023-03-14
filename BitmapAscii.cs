904e32e75bbc307a567009837604bdc0f6e214eb
william-c-bradford <william.bradford@mscoding.org>
2023-03-14 14:04:16 -0500

    Add project files.

---
+++ BitmapAscii.cs
@@ -0,0 +1,337 @@
+﻿using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System;
+
+class BitmapAscii
+{
+    // FIELDS
+    private string _asciiText = "";
+
+    #region
+    // PUBLIC METHODS
+    public string Asciitize (BitmapMaker bmpImage, int kernelWidth, int kernelHeight, int numberOfChars)
+    {
+        // This method should accept a BitmapMaker instance and return
+        // a string containing the ascii text version of the picture
+
+        // Create a BitmapAscii object
+        BitmapAscii bitmapAscii = new BitmapAscii();
+
+        // Create string to store ascii text
+        string asciiText = "";
+
+        // Loop through image
+        for (int i = 0; i < bmpImage.Height; i += kernelHeight)
+        {
+            for (int j = 0; j < bmpImage.Width; j += kernelWidth)
+            {
+                // Set height increment
+                int heightInc = i + kernelHeight;
+
+                // If height increment is greater than image height
+                if (heightInc > bmpImage.Height)
+                {
+                    // Set height increment to image height
+                    heightInc = bmpImage.Height;
+                }// End if
+
+                // Set width increment
+                int widthInc = j + kernelWidth;
+
+                // If width increment is greater than image width
+                if (widthInc > bmpImage.Width)
+                {
+                    // Set width increment to image width
+                    widthInc = bmpImage.Width;
+                }// End if
+
+                // Create a list to store colors
+                List<Color> colorList = new List<Color>();
+
+                // Loop through kernel
+                for (int y = i; y < heightInc; y++)
+                {
+                    for (int x = j; x < widthInc; x++)
+                    {
+                        // Get color of each pixel
+                        Color color = bmpImage.GetPixelColor(x, y);
+
+                        // Add color to color list
+                        colorList.Add(color);
+                    }// End for
+                }// End for
+
+                // Get the average color of the color list
+                double avgColorNum = bitmapAscii.AveragePixel(colorList);
+
+                // Get the ascii character from the value of the average color
+                string asciiChar = bitmapAscii.GrayToString(avgColorNum, numberOfChars);
+
+                // Add the ascii character to the ascii string
+                asciiText += asciiChar;
+            }// End for
+
+            // Start a new line in the ascii string
+            asciiText += "\n";
+        }// End for
+
+        // Store the asciiText in the private field
+        _asciiText = asciiText;
+
+        // Return the complete ascii string
+        return asciiText;
+    }// End Asciitize
+
+    public override string ToString()
+    {
+        // Overload the ToString() method to return a string containing
+        // the entire ascii version of the image
+
+        // Return the asciiText
+        return _asciiText;
+    }// End override ToString()
+    #endregion
+
+    #region
+    // PRIVATE METHODS
+    private double AveragePixel(int r, int g, int b)
+    {
+        // Average color's r,g,b value
+        double avg = (r + g + b) / 3;
+
+        // Normalize the average color
+        double avgNormalized = avg / 255;
+
+        // Return normalized average
+        return avgNormalized;
+    }// End AveragePixel (int, int, int)
+
+    private double AveragePixel(Color color)
+    {
+        // This method should accept components for a color instance and return
+        // a normalized value (0-1) of the grey value calculated from the RGB values
+
+        // Average color's r,g,b value
+        double avg = (color.R + color.G + color.B) / 3;
+
+        // Normalize the average color
+        double avgNormalized = avg / 255;
+
+        // Return normalized average
+        return avgNormalized;
+    }// End AveragePixel(Color)
+
+    private double AveragePixel(List<Color> colorList)
+    {
+        // This method should accept a list of Colors and return a normalized value
+        // (0-1) of the grey value calculated from the Colors RGB value
+
+        // Create variable to store total average
+        double totalAvg = 0;
+
+        // If list count is 0
+        if (colorList.Count == 0)
+        {
+            // Return default
+            return default;
+        }// End if
+        else
+        {
+            // Loop through each color in list
+            foreach (Color color in colorList)
+            {
+                // Average color's r,g,b value
+                double avg = (color.R + color.G + color.B) / 3;
+
+                // Add the average to the total
+                totalAvg += avg;
+            }// End foreach
+
+            // Divide the total average by number of colors added
+            totalAvg /= colorList.Count;
+
+            // Noramlize average
+            totalAvg /= 255;
+
+            // Return normalized total average
+            return totalAvg;
+        }// End else
+    }// End AveragePixel(Color)
+
+    private string GrayToString(double number, int numberOfChars)
+    {
+        // Should accept a normalized value (0-1) and return a string containing
+        // the ascii character mapped to the range the value falls in
+
+        // Check normalized value and return ascii character
+        if (numberOfChars == 0)
+        {
+            if (0 <= number && number < 0.16)
+            {
+                return "@";
+            }// End if
+            else if (number < 0.33)
+            {
+                return "%";
+            }// End else if
+            else if (number < 0.50)
+            {
+                return "*";
+            }// End else if
+            else if (number < 0.66)
+            {
+                return ":";
+            }// End else if
+            else if (number < 0.83)
+            {
+                return ".";
+            }// End else if
+            else if (number <= 1)
+            {
+                return " ";
+            }// End else if
+        }// End if
+
+        else if (numberOfChars == 1)
+        {
+            if (0 <= number && number < 0.10)
+            {
+                return "@";
+            }// End if
+            else if (number < 0.20)
+            {
+                return "%";
+            }// End else if
+            else if (number < 0.30)
+            {
+                return "$";
+            }// End else if
+            else if (number < 0.40)
+            {
+                return "#";
+            }// End else if
+            else if (number < 0.50)
+            {
+                return "X";
+            }// End else if
+            else if (number < 0.60)
+            {
+                return "*";
+            }// End else if
+            else if (number < 0.70)
+            {
+                return "+";
+            }// End else if
+            else if (number < 0.80)
+            {
+                return ":";
+            }// End else if
+            else if (number < 0.90)
+            {
+                return ".";
+            }// End else if
+            else if (number <= 1)
+            {
+                return " ";
+            }// End else if
+        }// End if
+
+        else if (numberOfChars == 2)
+        {
+            if (0 <= number && number < 0.05)
+            {
+                return "@";
+            }// End if
+            else if (0.05 <= number && number < 0.10)
+            {
+                return "%";
+            }// End else if
+            else if (0.10 <= number && number < 0.15)
+            {
+                return "$";
+            }// End else if
+            else if (0.15 <= number && number < 0.20)
+            {
+                return "#";
+            }// End else if
+            else if (0.20 <= number && number < 0.25)
+            {
+                return "8";
+            }// End else if
+            else if (0.25 <= number && number < 0.30)
+            {
+                return "X";
+            }// End else if
+            else if (0.30 <= number && number < 0.35)
+            {
+                return "G";
+            }// End else if
+            else if (0.35 <= number && number < 0.40)
+            {
+                return "9";
+            }// End else if
+            else if (0.40 <= number && number < 0.45)
+            {
+                return "S";
+            }// End else if
+            else if (0.45 <= number && number < 0.50)
+            {
+                return "Z";
+            }// End else if
+            else if (0.50 <= number && number < 0.55)
+            {
+                return "C";
+            }// End else if
+            else if (0.55 <= number && number < 0.60)
+            {
+                return "7";
+            }// End else if
+            else if (0.60 <= number && number < 0.65)
+            {
+                return "v";
+            }// End else if
+            else if (0.65 <= number && number < 0.70)
+            {
+                return "?";
+            }// End else if
+            else if (0.70 <= number && number < 0.75)
+            {
+                return "*";
+            }// End else if
+            else if (0.75 <= number && number < 0.80)
+            {
+                return "+";
+            }// End else if
+            else if (0.80 <= number && number < 0.85)
+            {
+                return ";";
+            }// End else if
+            else if (0.85 <= number && number < 0.90)
+            {
+                return ":";
+            }// End else if
+            else if (0.90 <= number && number < 0.95)
+            {
+                return ".";
+            }// End else if
+            else if (0.95 <= number && number <= 1)
+            {
+                return " ";
+            }// End else if
+        }// End if
+        
+        else
+        {
+            // If outside 0-1 value
+            throw new Exception("Value must be between 0 and 1");
+        }// End else
+
+        return string.Empty;
+    }// End GrayToString()
+    #endregion
+
+}// End class
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,300 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _3_6_Photoshop_ASCIIFY_Project
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        // Fields
+        private string? _loadedImage;
+        private int _numberOfChars = 0;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }// End MainWindow
+        
+        private void BtnASCIIFY_Click(object sender, RoutedEventArgs e)
+        {
+            // Clear the ascii text box
+            txtASCII.Text = string.Empty;
+
+            // Create new BitmapAscii object
+            BitmapAscii bitmapAscii = new BitmapAscii();
+
+            // If image has been loaded
+            if (_loadedImage != null )
+            {
+                // Set BitmapMaker object to loaded image
+                BitmapMaker bitmapMaker = new BitmapMaker(_loadedImage);
+                
+                // Convert the loaded image to grayscale
+                BitmapMaker bmpGrayscale = ConvertToGrayscale(bitmapMaker);
+
+                // Asciitize the bitmap image and store in string
+                string asciiString = bitmapAscii.Asciitize(bmpGrayscale, 
+                    Int32.Parse(txtKernelWidth.Text), Int32.Parse(txtKernelHeight.Text),
+                    _numberOfChars);
+            
+                // Display ascii string in ascii text box
+                txtASCII.Text += asciiString;
+            }// End if
+        }// End btnASCIIFY_Click()
+
+        private void BtnClearAll_Click(object sender, RoutedEventArgs e)
+        {
+            // Clear the image box
+            imgMain.Source = null;
+
+            // Clear the ascii text box
+            txtASCII.Text = string.Empty;
+
+            // Clear the loaded image path
+            _loadedImage = null;
+        }// End btnClearAll_Click()
+
+        private void BtnCopyAscii_Click(object sender, RoutedEventArgs e)
+        {
+            // If the ascii text box is not empty
+            if (txtASCII.Text != string.Empty)
+            {
+                // Copy the ascii text to the clipboard
+                Clipboard.SetText(txtASCII.Text);
+            }// End if
+        }// End btnCopyAscii_Click
+
+        private void FontPlus_Click(object sender, RoutedEventArgs e)
+        {
+            // Get the font size
+            int fontSizeNum = Int32.Parse(txtFontSize.Text);
+
+            // Font maximum 100
+            if (fontSizeNum < 100)
+            {
+                // Increase font size
+                fontSizeNum++;
+            }// End if
+
+            // Display the font size in the text box
+            txtFontSize.Text = fontSizeNum.ToString();
+
+            // Change the font size of the ascii text box
+            txtASCII.FontSize = fontSizeNum;
+        }// End fontPlus_Click()
+
+        private void FontMinus_Click(object sender, RoutedEventArgs e)
+        {
+            // Get the font size
+            int fontSizeNum = Int32.Parse(txtFontSize.Text);
+
+            // Font minimum 1
+            if (fontSizeNum > 1)
+            {
+                // Decrease font size
+                fontSizeNum--;
+            }// End if
+
+            // Display the font size in the text box
+            txtFontSize.Text = fontSizeNum.ToString();
+
+            // Change the font size of the ascii text box
+            txtASCII.FontSize = fontSizeNum;
+        }// End fontMinus_Click()
+
+        private void MuiOpen_Click(object sender, RoutedEventArgs e)
+        {
+            // Create open file dialog
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            // Setup parameters for open file dialog
+            openFileDialog.DefaultExt = ".png";
+            openFileDialog.Filter = "PNG Files (.png)|*.png";
+
+            // Show file dialog
+            bool? result = openFileDialog.ShowDialog();
+
+            // Process dialog results to determine if a file was opened
+            if (result == true)
+            {
+                // Store file path
+                _loadedImage = openFileDialog.FileName;
+
+                // Call LoadImage method
+                LoadImage(_loadedImage);
+
+                // Clear the ascii text box
+                txtASCII.Text = string.Empty;
+            }// End if
+        }// End muiOpen_Click()
+
+        private void LoadImage(string path)
+        {
+            // Create a bitmap image to load the data
+            BitmapMaker bmpImage = new BitmapMaker(path);
+
+            // Create a grayscale copy of the bitmap
+            BitmapMaker bmpCopy = ConvertToGrayscale(bmpImage);
+
+            // Create a new bitmap of the original image
+            WriteableBitmap wbmImage = bmpImage.MakeBitmap();
+
+            // Set image control to display the original image
+            imgMain.Source = wbmImage;
+        }// End LoadImage()
+
+        public static BitmapMaker ConvertToGrayscale(BitmapMaker bmpOriginal)
+        {
+            // Create a blank bitmap the same size as the original image
+            BitmapMaker bmpNewImage = new BitmapMaker(bmpOriginal.Width, bmpOriginal.Height);
+
+            // Loop through all the pixels in the original image
+            for (int x = 0; x < bmpOriginal.Width; x++)
+            {
+                for (int y = 0; y < bmpOriginal.Height; y++)
+                {
+                    // Get the color of the pixel at this location
+                    Color originalColor = bmpOriginal.GetPixelColor(x, y);
+
+                    // Calculate the grayscale value of the pixel
+                    int grayScale = (int)((originalColor.R * 0.3) + (originalColor.G * 0.59) + (originalColor.B * 0.11));
+
+                    // Convert the grayscale value to a byte
+                    byte grayScaleByte = (byte)grayScale;
+
+                    // Create a new color using the grayscale value for each component
+                    Color grayScaleColor = Color.FromArgb(originalColor.A, grayScaleByte, grayScaleByte, grayScaleByte);
+
+                    // Set the pixel in the new bitmap to the new color
+                    bmpNewImage.SetPixel(x, y, grayScaleColor);
+                }// End for
+            }// End for
+
+            // Return the new grayscale image
+            return bmpNewImage;
+        }// End ConvertToGrayscale()
+
+        private void PlusKernelWidth_Click(object sender, RoutedEventArgs e)
+        {
+            // Get the kernel width
+            int kernelWidthNum = Int32.Parse(txtKernelWidth.Text);
+
+            // Kernel width maximum 30
+            if (kernelWidthNum < 30)
+            {
+                // Increase kernel width
+                kernelWidthNum++;
+            }// End if
+
+            // Display the kernel width in the text box
+            txtKernelWidth.Text = kernelWidthNum.ToString();
+        }// End plusKernelWidth_Click()
+
+        private void MinusKernelWidth_Click(object sender, RoutedEventArgs e)
+        {
+            // Get the kernel width
+            int kernelWidthNum = Int32.Parse(txtKernelWidth.Text);
+
+            // Kernel width minimum 1
+            if (kernelWidthNum > 1)
+            {
+                // Decrease kernel width
+                kernelWidthNum--;
+            }// End if
+
+            // Display the kernel width in the text box
+            txtKernelWidth.Text = kernelWidthNum.ToString();
+        }// End minusKernelWidth_Click()
+
+        private void PlusKernelHeight_Click(object sender, RoutedEventArgs e)
+        {
+            // Get the kernel height
+            int kernelHeightNum = Int32.Parse(txtKernelHeight.Text);
+
+            // Kernel height maximum 30
+            if (kernelHeightNum < 30)
+            {
+                // Increase kernel height
+                kernelHeightNum++;
+            }// End if
+
+            // Display the kernel height in the text box
+            txtKernelHeight.Text = kernelHeightNum.ToString();
+        }// End plusKernelHeight_Click()
+
+        private void MinusKernelHeight_Click(object sender, RoutedEventArgs e)
+        {
+            // Get the kernel height
+            int kernelHeightNum = Int32.Parse(txtKernelHeight.Text);
+
+            // Kernel height minimum 1
+            if (kernelHeightNum > 1)
+            {
+                // Decrease kernel height
+                kernelHeightNum--;
+            }// End if
+
+            // Display the kernel height in the text box
+            txtKernelHeight.Text = kernelHeightNum.ToString();
+        }// End minusKernelHeight_Click()
+
+        private void CharPlus_Click(object sender, RoutedEventArgs e)
+        {
+            // If characters used are not max
+            if (_numberOfChars < 2)
+            {
+                // Increase number of characters used
+                _numberOfChars++;
+
+                // Set the number of characters text box
+                UpdateCharNumText();
+            }// End if
+        }// End charPlus_Click()
+
+        private void CharMinus_Click(object sender, RoutedEventArgs e)
+        {
+            // If characters used are not min
+            if (_numberOfChars > 0)
+            {
+                // Decrease number of characters used
+                _numberOfChars--;
+
+                // Set the number of characters text box
+                UpdateCharNumText();
+            }// End if
+        }// End charMinus_Click()
+
+        private void UpdateCharNumText()
+        {
+            // Check number of characters
+            if (_numberOfChars == 0)
+            {
+                txtCharNum.Text = "6";
+            }// End if
+            else if (_numberOfChars == 1)
+            {
+                txtCharNum.Text = "10";
+            }// End else if
+            else if (_numberOfChars == 2)
+            {
+                txtCharNum.Text = "20";
+            }// End else if
+        }// End CharNumberText()
+    }// End class
+}// End namespace
